b1fedff1fe3f8140100ed639bc4e836602e52a17
AlaaKhaledAhmed <97679146+AlaaKhaledAhmed@users.noreply.github.com>
2022-02-01 09:58:51 +0300

    first

---
+++ lib/Account/logging.dart
@@ -0,0 +1,15 @@
+import"package:flutter/material.dart";
+
+class Logging extends StatefulWidget {
+  Logging({Key? key}) : super(key: key);
+
+  @override
+  State<Logging> createState() => _LoggingState();
+}
+
+class _LoggingState extends State<Logging> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container());
+  }
+}
