416900245274d3e1efa0ab3da1ffceae7c8abea0
AlaaKhaledAhmed <97679146+AlaaKhaledAhmed@users.noreply.github.com>
2022-02-06 08:38:07 +0300

    FFF

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           theme: ThemeData(fontFamily: "Cairo"),
           home:Logging()
+          //----------------------------------
         ),
     );
   }
